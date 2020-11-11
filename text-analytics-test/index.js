1322a55cda7fcd32271a8cba5a890f00d1dd055e
Joe D'Agostino <joedag32@gmail.com>
2020-11-11 14:14:08 -0500

    can see if key terms match privacy terms

--- text-analytics-test/index.js
+++ text-analytics-test/index.js
@@ -1,6 +1,15 @@
 /* declare some variables */
 const functionURL =
   "https://eastus.api.cognitive.microsoft.com/text/analytics/v3.0/keyPhrases";
+// array of privacy terms that we are looking for a match on
+const privacyTerms = [
+  "address",
+  "cookies",
+  "credit card",
+  "ip address",
+  "personal data",
+  "third-party"
+];
 const input = document.getElementById("testValue");
 const analyzeButton = document.getElementById("analyzeButton");
 const objectOutput = document.getElementById("returnedObject");
@@ -37,10 +46,43 @@ function analyzeText(e) {
   })
     .then(res => res.json())
     .then(data => {
-      console.log(data);
-      objectOutput.innerHTML = data.documents[0].keyPhrases;
+      evaluateKeyPhrases(data);
     })
     .catch(error => {
       console.error(error);
     });
 }
+
+// evaluate the returend Key Phrases
+function evaluateKeyPhrases(data) {
+  objectOutput.innerHTML = "";
+  let keyPhrasesArray = data.documents[0].keyPhrases;
+
+  // check to see if Key Phrases match any of our Privacy Terms
+  let privacyTermMatches = 0;
+  keyPhrasesArray.forEach(phrase => {
+    privacyTerms.forEach(term => {
+      if (term.toLowerCase() === phrase.toLowerCase()) {
+        privacyTermMatches++;
+      }
+    });
+  });
+  if (privacyTermMatches) {
+    objectOutput.innerHTML +=
+      "<h1>Matched Privacy Terms " + privacyTermMatches + " times!!</h1>";
+  }
+
+  // output phrases for web app demo
+  objectOutput.innerHTML += "<ul>";
+  keyPhrasesArray.forEach(
+    element => (objectOutput.innerHTML += "<li>" + element + "</li>")
+  );
+  objectOutput.innerHTML += "</ul>";
+  objectOutput.innerHTML += "<h2>Privacy Terms</h2>";
+  objectOutput.innerHTML += "<ul>";
+  privacyTerms.forEach(
+    element => (objectOutput.innerHTML += "<li>" + element + "</li>")
+  );
+  objectOutput.innerHTML += "</ul>";
+}
+
